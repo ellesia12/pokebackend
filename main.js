7e96ac9c554ada0905b2f2bdfad17d220ecb7029
Ellesia Janto <jantoellesia@gmail.com>
2020-11-07 09:56:22 +0100

    serving the static folder

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 const app = express()
 //fs is needed to serve JSON file
 const fs = require('fs');
 const port = process.env.PORT || 8080
 //don't forget the routes
 const pokeRoute = require('./routes/pokeRoute')
+
+// serving the static folder
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
 // configure express instance w/ some body-parser settings, including handling JSON data
